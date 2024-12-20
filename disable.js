02b1322ec0f019e1d953d2b890cd42f0373e0daf
Ludovico Caluori <119592512+ololiuhqui@users.noreply.github.com>
2024-12-20 12:40:30 +0100

    Create disable.js

---
+++ disable.js
@@ -0,0 +1,14 @@
+let TLDs = document.querySelectorAll(".modal-content .list-group-item");
+let names = document.querySelectorAll(".modal-content .list-group-item .notranslate");
+let buttons = document.querySelectorAll(".modal-content .list-group-item button");
+
+for (let i = 0; i <= TLDs.length; i++){
+setTimeout(function timer() {
+    if (buttons[i].classList.contains("btn-primary")){
+    buttons[i].click();
+    console.log(`new TLD ${names[i].innerHTML} disabled!`);
+    } else {
+        console.log(`${names[i].innerHTML} already disabled, skipping to next, do not close the page...`)
+    }
+  }, i * 500);
+}

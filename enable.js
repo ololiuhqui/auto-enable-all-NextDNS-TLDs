a991a2153ec20ec841946eb6a2a1e2c414d4fa98
Ludovico Caluori <119592512+ololiuhqui@users.noreply.github.com>
2024-12-20 12:39:45 +0100

    Create enable.js

---
+++ enable.js
@@ -0,0 +1,14 @@
+let TLDs = document.querySelectorAll(".modal-content .list-group-item");
+let names = document.querySelectorAll(".modal-content .list-group-item .notranslate");
+let buttons = document.querySelectorAll(".modal-content .list-group-item button");
+
+for (let i = 0; i <= TLDs.length; i++){
+setTimeout(function timer() {
+    if (buttons[i].classList.contains("btn-danger")){
+    buttons[i].click();
+    console.log(`new TLD ${names[i].innerHTML} enabled!`);
+    } else {
+        console.log(`${names[i].innerHTML} already enabled, skipping to next, do not close the page...`)
+    }
+  }, i * 500);
+}
